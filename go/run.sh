52e7735dc876776a2deb2fc99cc1f5c2ba550f2c
Gabriel Gutierrez <gabri.gutiz@gmail.com>
2024-06-28 15:50:20 -0300

    [Go] ref: edit ./run.sh

--- go/run.sh
+++ go/run.sh
@@ -4,12 +4,9 @@ FILE="main"
 
 if [ -f "$FILE" ]; then
     rm "$FILE"
-    echo "File $FILE deleted."
-else
-    echo "File $FILE does not exist."
 fi
 
 go build main.go
 
-time ./main -input $1
+time ./main -input $1 -cpuprofile cpu.prof
 
